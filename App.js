9fe5de0fba90282cfb7005a827888d2e48db16c6
edu2969 <edtronco@gmail.com>
2021-08-31 12:15:03 -0400

    Commit inicial de pruebas
    - Probando cosillas

--- App.js
+++ App.js
@@ -1,21 +1,76 @@
+import { LinearGradient } from 'expo-linear-gradient';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TextInput, View } from 'react-native';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+  const [loaded] = useFonts({
+    Playball: require('./assets/fonts/Playball-Regular.ttf'),
+  });
+
+  const [value, onChangeText] = React.useState('Useless Placeholder');
+
+  if (!loaded) {
+    return null;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.layout}>
+      <LinearGradient
+        colors={['rgba(42, 107, 255, 1)', 'rgba(67, 65, 255, 1)']}
+        style={styles.background}
+      />
+      <Image style={styles.backImage}
+        source={require('./assets/backgrounds/background-login.png')} />
+      <View style={styles.content}>
+        <Image style={styles.logo} source={require('./assets/logo.png')} />
+        <Text style={styles.brand}>AnclaFit</Text>
+        <TextInput
+          onChangeText={text => onChangeText(text)}
+          value={value}
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  layout: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    flexDirection: 'row',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  content: {
+    flex: 1,
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    height: '100%',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  background: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    height: '100%',
+  },
+  backImage: {
+    width: '100%',
+    height: '100%',
+    opacity: 0.1,
+  },
+  brand: {
+    color: 'white',
+    fontSize: 40,
+    fontFamily: 'Playball',
+  },
+  logo: {
+    width: 150,
+    height: 150,
   },
 });
